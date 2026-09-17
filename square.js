c3b79647f2e5be8af3003fb7576d48b5415a61b8
yusif kofi sadick <124945382+yusifkofisadick@users.noreply.github.com>
2026-09-17 16:28:02 -0700

    Add files via upload

---
+++ square.js
@@ -0,0 +1,12 @@
+// Function to calculate the area of a square
+function areaOfSquare(sideLength) {
+    // Calculate the area of a square given the length of its side
+    var area = sideLength * sideLength;
+    // Return the calculated area
+    return area;
+}
+
+// Example usage of the areaOfSquare function
+var area = areaOfSquare(5);
+// Output the area of the square
+console.log("The area of the square is: " + area);
