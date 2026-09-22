46ab65e484e5e55d2756b66daf1571f1826f1309
yusif kofi sadick <124945382+yusifkofisadick@users.noreply.github.com>
2026-09-22 02:03:52 -0700

    Add guest name to guest list with prompt
    Prompt user for a name and add it to the guest list if not already included.

---
+++ guestList.js
@@ -0,0 +1,9 @@
+var guestList = ["Kofi","Yusif","Quansah"]
+
+var guestName = prompt("Enter name here")
+
+//adds a geust to guest list
+guestList.includes(guestName)
+
+alert(guestName + " added to list.")
+
